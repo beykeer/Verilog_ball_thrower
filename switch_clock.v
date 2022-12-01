8e8e55f089945342aa7818b41ccbb52ed0c777ec
beykeer <119363478+beykeer@users.noreply.github.com>
2022-12-01 18:04:23 -0500

    Add files via upload

---
+++ switch_clock.v
@@ -0,0 +1,26 @@
+module switch_clock(clk, update,VGA_CLK);
+input clk;
+output reg update;
+output reg VGA_CLK;
+reg [21:0]count;	
+reg i;
+	
+//controls how fast the player and the blocks update
+always@(posedge board_clk)
+	begin
+		count <= count + 22'd1;
+		if(count == 22'd1250000)
+		begin
+			update <= ~update;
+			count <= 22'd0;
+		end	
+	end
+	
+//converts the board clock (50MHz) to  the VGA clock (25MHz)
+always@(posedge board_clk)
+	begin
+		i <= ~i; 
+		VGA_CLK <= i;
+	end
+
+endmodule 
