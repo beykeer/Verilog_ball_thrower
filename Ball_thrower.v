60b558d18351a808432eed6ace558ab76cc1970e
beykeer <119363478+beykeer@users.noreply.github.com>
2022-12-02 13:28:22 -0500

    Add files via upload

--- Ball_thrower.v
+++ Ball_thrower.v
@@ -3,7 +3,7 @@ module Ball_thrower(
   input         CLOCK_50,
 
 // Push Button
-  input  [3:0]  KEY;         // Pushbutton[3:0]
+  input  [3:0] KEY,         // Pushbutton[3:0]
 
   // DPDT Switch
   input  [17:0] SW,          // Toggle Switch[17:0]
@@ -18,8 +18,6 @@ module Ball_thrower(
   output reg [7:0]  VGA_G,       // VGA Green[9:0]
   output reg [7:0]  VGA_B       // VGA Blue[9:0]
   );
-  
-  
 wire clk;
 wire rst;
 
@@ -27,7 +25,7 @@ assign clk = CLOCK_50;
 assign rst = SW[17];
 
 
-wire VGA_CLK; 
+//reg VGA_CLK; 
 wire update;
 
 switch_clock clocks(clk, update, VGA_CLK); //converts the board clock to VGA clock and frame update clock
@@ -47,7 +45,7 @@ assign powerdown = KEY[3];
 
 angle_power aandp(VGA_CLK, rst, angleup, angledown, powerup, powerdown, update, xCount, yCount, arrow);
 
-
+//ball(VGA_CLK,rst,update,xCount,yCount,ball);
 
 //display
 wire [9:0] xCount; 
--- ball.v
+++ ball.v
@@ -1,4 +1,22 @@
-module ball(clk, rst, update, xlength, ylength, ball);
+module ball(clk,rst,update,xCount,yCount,,,ball);
+
+input clk, rst, update;
+input [9:0]xCount;
+input [9:0]yCount;
+output ball;
+
+reg ball;
+wire rst;
+
+always @(posedge clk)
+begin
+	ball = (xCount > ballX[0] && xCount < (ballX[0] + 10'd10)) && (yCount > ballY[0] && yCount < (ballY[0] + 10'd10));
+
+end
+
+reg[9:0] ballX [0:9];
+reg [8:0]ballY [0:9]; 
+
 
 
 
