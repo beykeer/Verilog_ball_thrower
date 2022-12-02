1969faafb6e7d1508f8f077e7cc7b406a22113f1
beykeer <119363478+beykeer@users.noreply.github.com>
2022-12-02 12:58:36 -0500

    Update angle_power.v

--- angle_power.v
+++ angle_power.v
@@ -3,8 +3,8 @@ input clk, rst, angleup, angledown, powerup, powerdown, update;
 input [9:0]xCount;
 input [9:0]yCount;
 output arrow;
-output [2:0]Vel;
-output [4:0]Ang;
+output reg[2:0]Vel;
+output reg[4:0]Ang;
 
 reg arrow;
 wire rst;
@@ -42,7 +42,7 @@ always @(*)
 	case (S)
 		ANGLEUP:
 			begin
-				if(angleup == 1'b0)
+				if(angleup == 1'b0 && Ang < 5'd16)
 					NS = ANGLEUP;
 					
 				else
@@ -51,34 +51,34 @@ always @(*)
 			
 		ANGLEDOWN:
 			begin
-				if(angledown == 1'b0)
+				if(angledown == 1'b0 && Ang > 5'd0)
 					NS = ANGLEDOWN;
 				else
 					NS = STAY;
 			end
 		POWERUP:
 			begin
-				if(powerup == 1'b0)
+				if(powerup == 1'b0 && Vel < 3'd5)
 					NS = POWERUP;
 				else
 					NS = STAY;
 			end
 		POWERDOWN:
 			begin
-				if(powerdown == 1'b0)
+				if(powerdown == 1'b0 && Vel > 3'd0)
 					NS = POWERDOWN;
 				else
 					NS = STAY;
 			end
 		STAY: 
 			begin
-				if(angleup == 1'b0)
+				if(angleup == 1'b0 && Ang < 5'd16)
 					NS = ANGLEUP;
-				else if (angledown == 1'b0)
+				else if (angledown == 1'b0 && Ang > 5'd0)
 					NS = ANGLEDOWN;
-				else if (powerup == 1'b0)
+				else if (powerup == 1'b0 && Vel < 3'd5)
 					NS = POWERUP;
-				else if (powerdown == 1'b0)
+				else if (powerdown == 1'b0 && Vel > 3'd0)
 					NS = POWERDOWN;
 				else
 					NS = STAY;
@@ -91,6 +91,8 @@ always @(*)
 		begin //starting position
 			arrowX[0] <= 10'd31;
 			arrowY[0] <= 9'd443;
+			Vel <= 3'd0;
+			Ang <= 5'd0;
 		end
 	else
 		case(S)
@@ -98,26 +100,32 @@ always @(*)
 				begin
 					arrowX[0] <= (arrowX[0] - 10'd1);
 					arrowY[0] <= (arrowY[0] - 9'd4);
+					Ang <= Ang + 5'd1;
 				end
 			ANGLEDOWN:
 				begin
 					arrowX[0] <= (arrowX[0] + 10'd1);
 					arrowY[0] <= (arrowY[0] + 9'd4);
+					Ang <= Ang - 5'd1;
 				end
 			POWERUP:
 				begin
 					arrowX[0] <= (arrowX[0] + 10'd4);
 					arrowY[0] <= (arrowY[0] - 9'd10);
+					Vel <= Vel + 3'd1;
 				end
 			POWERDOWN:
 				begin
 					arrowX[0] <= (arrowX[0] - 10'd4);
 					arrowY[0] <= (arrowY[0] + 9'd10);
+					Vel <= Vel + 3'd1;
 				end
 			STAY:
 				begin
 					arrowX[0] <= arrowX[0];
 					arrowY[0] <= arrowY[0];
+					Vel <= Vel;
+					Ang <= Ang;
 				end
 		endcase
 	end
